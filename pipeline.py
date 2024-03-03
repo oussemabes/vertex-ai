2c7c90557175875f1255ddbbaa41d197836cf813
oussemabes <oussema.besbes@supcom.tn>
2024-03-03 20:32:32 +0100

    hh

--- pipeline.py
+++ pipeline.py
@@ -254,7 +254,7 @@ parameters = hyperparameter_tuning_job.serialize_parameters({
 # Define the hyperparameter tuning job
 def tuning_job(project: str, display_name: str, hp_dict: str, data_dir: str):
     
-    hyperparameter_tuning_job.HyperparameterTuningJobRunOp(
+    tuning_op =hyperparameter_tuning_job.HyperparameterTuningJobRunOp(
     display_name=display_name,
     base_output_directory=base_output_directory,
     worker_pool_specs=worker_pool_specs,
@@ -264,7 +264,7 @@ def tuning_job(project: str, display_name: str, hp_dict: str, data_dir: str):
     parallel_trial_count=3,  # Example value, adjust as needed
     project=project_number
 )
-    return tuning_job.outputs["best_trial_parameters"]
+    return tuning_op.outputs["best_trial_parameters"]
 
 # Define the training job with hyperparameters as arguments
 def training_job(project: str, display_name: str, data_dir: str, best_hyperparameters: dict):
